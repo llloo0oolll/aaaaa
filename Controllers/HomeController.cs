73e3e2309481727cbecec65a70e9f7c936ae711f
llloo0oolll <58760038+llloo0oolll@users.noreply.github.com>
2021-06-15 14:15:54 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Services.Exceptions;
+using starter_web_api.Models;
+
+namespace starter_web_api.Controllers
+{
+    public class HomeController : DnnApiController
+    {
+        [DnnAuthorize()]
+        [HttpGet()]
+        public HttpResponseMessage DnnHello()
+        {
+            try
+            {
+                string dnnHello = "Hello from Dnn!";
+                return Request.CreateResponse(HttpStatusCode.OK, dnnHello);
+            }
+            catch (System.Exception ex)
+            {
+                //Log exception and reply with Error
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [DnnAuthorize()]
+        [HttpPost()]
+        [ValidateAntiForgeryToken()]
+        public HttpResponseMessage DnnHelloPersonalize(DetailsModel data)
+        {
+            try
+            {
+                string dnnMessage = "Hello " + data.name + " from DNN!";
+                return Request.CreateResponse(HttpStatusCode.OK, dnnMessage);
+            }
+            catch (System.Exception ex)
+            {
+                //Log exception and reply with Error
+                Exceptions.LogException(ex);
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
